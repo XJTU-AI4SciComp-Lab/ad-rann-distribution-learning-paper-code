2cabacf38afcfe1893610092ed86b82318a50f62
nightmare-droag <19848185798@163.com>
2026-08-02 18:28:13 +0800

    Delete examples directory

--- examples/poisson_lshape_2d/run_mixture_poisson_lshape_2d.m
+++
@@ -1,9 +0,0 @@
-clear;
-clc;
-close all;
-
-this_dir = fileparts(mfilename('fullpath'));
-addpath(this_dir,'-begin');
-
-[cfg,project_root] = config_poisson_lshape_mixture();
-results = poisson_lshape_mixture_study(cfg,project_root);
